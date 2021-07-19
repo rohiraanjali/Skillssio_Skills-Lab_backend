f765964dd0d39f1113ceac127febf924163c45a2
Anjali Rohira <rohiraanjali@gmail.com>
2021-07-19 09:54:05 +0530

    updated history.controller.js

--- server/controllers/history.controller.js
+++ server/controllers/history.controller.js
@@ -1,40 +1,36 @@
 const Users = require("../models/users.model");
 
 const addToHistory = async(req, res) => {
-const {uid, videoId} = req.params;
-const user = await Users.findOne({_id: uid})
-
-if(user.history.find(vid => vid == videoId)) {
-user.history.remove(videoId);
-user.history.push(videoId);
-
-console.log(user.history)
-
-await user.save (async(err, user) => {
-    if(user) {
-        const {history} = await user.execPopulate({path: "history", populate: "video"});
-        // console.log("video added successfully")
-        return res.status(200).json({success: true, history, message: "Video added successfully"})
+    const {uid,videoId} = req.params;
+    const user = await Users.findOne({_id:uid});
+ 
+    if(user.history.find(vid => vid == videoId)){
+        console.log("log ho ra")
+        await user.history.remove(videoId);
+        user.history.push(videoId);
+        await user.save(async(err,user) => {
+            if(user){
+               const {history} = await user.execPopulate({path:"history",populate:"videos"});
+               return res.status(200).json({success: true,history,message:"video added successfully"});
+            }
+            if(err){
+               return res.status(500).json({success:false,message:"Internal server error"});
+            }
+        });
+        return;
     }
-    if(err) {
-        // console.log("video not added successfully")
-        return res.status(500).json({success: false, message: "Internal server error. Try adding again"})
-    }
-});
-return;
-}
-user.history.push(videoId);
-await user.save(async(err,user) => {
-if(err){
-    return res.status(500).json({success:false,message:"Internal server error"})
-}
-if(user){
-    const {history} = await user.execPopulate({path:"history",populate:"video"});
-    return res.status(200).json({success: true,history,message:"video added successfully"});
-}
-});
-}
-
+ 
+    user.history.push(videoId);
+    await user.save(async(err,user) => {
+       if(err){
+          return res.status(505).json({success:false,message:"Internal server error"})
+       }
+       if(user){
+         const {history} = await user.execPopulate({path:"history",populate:"videos"});
+         return res.status(200).json({success: true,history,message:"video added successfully"});
+       }
+    });
+ }
 
 const removeFromHistory = async(req, res) => {
     const {uid,videoId} = req.params;
